a5326dc329c72c56828e818eddb83a67d159a5e8
connorbrooks18 <connorbrooks18@icloud.com>
2026-09-04 15:34:32 -0500

    functional

--- oa.py
+++ oa.py
@@ -81,7 +81,7 @@ class OmegaAutomaton:
         
         # Translate the LTL formula to an OA using Rabinizer 4.
         
-        out=check_output(['ltl2ldba', '-d', '-e', ltl] if self.oa_type == 'ldba' else ['ltl2dra', '-c', ltl], shell=True)
+        out=check_output(['ltl2ldba', '-e', ltl] if self.oa_type == 'ldba' else ['ltl2dra', '-c', ltl])
         
         # Split the output into two parts: the header and the body
         header, body = out.decode('utf-8').split('--BODY--\n')
@@ -161,7 +161,7 @@ class OmegaAutomaton:
         if spot:
             filename = self.random_hoa_filename()
             with open(filename,'wb') as f:
-                f.write(check_output(['ltl2ldba', '-d', ltl] if self.oa_type == 'ldba' else ['ltl2dra', '-c', ltl]))
+                f.write(check_output(['ltl2ldba', ltl] if self.oa_type == 'ldba' else ['ltl2dra', '-c', ltl]))
             spot.setup()
             spot_oa = spot.automaton(filename)
             spot_oa.merge_edges()  # For better visualization
